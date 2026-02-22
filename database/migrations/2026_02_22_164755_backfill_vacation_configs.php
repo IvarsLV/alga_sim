82be0fe871ab8c0f5c5e07e82d7e53b960116f59
IvarsLV <ivars.plisko@gmail.com>
2026-02-22 18:48:32 +0200

    chore: add migration to backfill vacation config rules with correct JSON
    - Fills the empty rules column for existing vacation type records in production databases.

---
+++ database/migrations/2026_02_22_164755_backfill_vacation_configs.php
@@ -0,0 +1,134 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $configs = [
+            1 => [
+                'measure_unit' => 'DD',
+                'accrual_method' => 'monthly',
+                'accrual_start' => 'from_hire',
+                'period_type' => 'working_year',
+                'shifts_working_year' => true,
+                'shifts_working_year_threshold_weeks' => 4,
+                'payment_status' => 'apmaksāts',
+                'financial_formula' => 'average_salary',
+                'law_reference' => 'DL 149',
+            ],
+            2 => [
+                'measure_unit' => 'DD',
+                'accrual_method' => 'yearly',
+                'accrual_start' => 'from_hire',
+                'period_type' => 'working_year',
+                'shifts_working_year' => false,
+                'payment_status' => 'neapmaksāts',
+                'financial_formula' => 'unpaid',
+                'law_reference' => 'DL 156',
+            ],
+            3 => [
+                'measure_unit' => 'DD',
+                'accrual_method' => 'yearly',
+                'accrual_start' => 'from_hire',
+                'period_type' => 'working_year',
+                'shifts_working_year' => false,
+                'payment_status' => 'apmaksāts',
+                'financial_formula' => 'base_salary',
+                'law_reference' => 'DL 157',
+            ],
+            4 => [
+                'measure_unit' => 'DD',
+                'accrual_method' => 'on_request',
+                'accrual_start' => 'immediate',
+                'period_type' => 'working_year',
+                'shifts_working_year' => true,
+                'shifts_working_year_threshold_weeks' => 4,
+                'payment_status' => 'neapmaksāts',
+                'financial_formula' => 'unpaid',
+                'law_reference' => 'DL 153',
+            ],
+            5 => [
+                'measure_unit' => 'DD',
+                'accrual_method' => 'yearly',
+                'accrual_start' => 'from_hire',
+                'period_type' => 'calendar_year',
+                'shifts_working_year' => false,
+                'expires_end_of_period' => true,
+                'child_based' => true,
+                'payment_status' => 'apmaksāts',
+                'financial_formula' => 'average_salary',
+                'law_reference' => 'DL 150-151',
+            ],
+            6 => [
+                'measure_unit' => 'KD',
+                'accrual_method' => 'on_request',
+                'accrual_start' => 'from_document',
+                'period_type' => 'working_year',
+                'shifts_working_year' => false,
+                'payment_status' => 'VSAA',
+                'financial_formula' => 'unpaid',
+                'law_reference' => 'DL 154',
+            ],
+            7 => [
+                'measure_unit' => 'DD',
+                'accrual_method' => 'per_event',
+                'event_source' => 'child_registration',
+                'event_days' => 10,
+                'requires_hire_date_check' => true,
+                'accrual_start' => 'from_document',
+                'period_type' => 'working_year',
+                'shifts_working_year' => false,
+                'usage_deadline_months' => 2,
+                'payment_status' => 'VSAA',
+                'financial_formula' => 'unpaid',
+                'law_reference' => 'DL 155',
+            ],
+            8 => [
+                'measure_unit' => 'DD',
+                'accrual_method' => 'per_event',
+                'event_source' => 'donor_day',
+                'event_days' => 1,
+                'accrual_start' => 'from_document',
+                'period_type' => 'working_year',
+                'shifts_working_year' => false,
+                'usage_deadline_days' => 30,
+                'payment_status' => 'apmaksāts',
+                'financial_formula' => 'average_salary',
+                'law_reference' => 'DL 74',
+            ],
+            9 => [
+                'measure_unit' => 'DD',
+                'accrual_method' => 'on_request',
+                'accrual_start' => 'immediate',
+                'period_type' => 'working_year',
+                'shifts_working_year' => false,
+                'payment_status' => 'neapmaksāts',
+                'financial_formula' => 'unpaid',
+                'law_reference' => 'DL 149',
+            ]
+        ];
+
+        foreach (DB::table('vacation_configs')->get() as $config) {
+            if (isset($configs[$config->tip])) {
+                $rulesJson = json_encode($configs[$config->tip]);
+                DB::table('vacation_configs')
+                    ->where('id', $config->id)
+                    ->update(['rules' => $rulesJson]);
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // No down needed
+    }
+};
